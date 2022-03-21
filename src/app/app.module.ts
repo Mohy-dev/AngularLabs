25953957654c64e63f39ac1c2b9c9535f9dc46ca
mohy-dev <mohy.dev@gmail.com>
2022-03-21 05:55:19 +0200

    Create first and second comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { SecondComponent } from './second/second.component';
+import { FirstComponent } from './first/first.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondComponent,
+    FirstComponent,
+    FormControl
   ],
   imports: [
     BrowserModule
