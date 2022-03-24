e0fb3d8a538cb69411c10a78d8b381284478ede4
mohy-dev <mohy.dev@gmail.com>
2022-03-24 15:21:22 +0200

    lab4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,30 @@ import { RegstComponent } from './mainAction/regst/regst.component';
 import { TableComponent } from './mainAction/table/table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { HeaderComponent } from './header/header.component'
+import { HeaderComponent } from './Routing/header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Routing/about/about.component';
+import { DetailsComponent } from './Routing/details/details.component';
+import { ErrorComponent } from './Routing/error/error.component';
+
+
+let routes:Routes = [
+  {path:"reg", component:RegstComponent},
+  {path:"table", component:TableComponent},
+  {path:"about", component:AboutComponent},
+  {path:"id", component:DetailsComponent},
+  {path:"**", component:ErrorComponent}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
     RegstComponent,
     TableComponent,
     HeaderComponent,
+    AboutComponent,
+    DetailsComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,7 +38,7 @@ import { HeaderComponent } from './header/header.component'
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
