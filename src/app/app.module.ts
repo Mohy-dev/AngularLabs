d8cd24f01e61179382526db523e662af52a62057
mohy-dev <mohy.dev@gmail.com>
2022-03-24 13:44:34 +0200

    lab4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { RegstComponent } from './mainAction/regst/regst.component';
 import { TableComponent } from './mainAction/table/table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component'
 @NgModule({
   declarations: [
     AppComponent,
     RegstComponent,
     TableComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
